0cc6e0fecedc8e699029d536834323d9ef3a3a20
unknown <manoel.leao@MVBRFORDSK7690.MVFOR.local>
2016-08-10 17:20:57 -0300

    commit

---
+++ TestaEmpresa.java
@@ -0,0 +1,28 @@
+package Caelum_4_5;
+
+public class TestaEmpresa {
+
+	 public static void main(String[]args){
+	  Empresa em = new Empresa();
+	  em.empregado = new Funcionario[5];
+	  
+	  Funcionario f1 = new Funcionario();
+	  f1.nome = "João";
+	  f1.salario = 1000;
+	  f1.departamento = "Departamento de vendas";
+	  em.adiciona(f1);
+	  
+	  Funcionario f2 = new Funcionario();
+	  f2.nome = "Carlos";
+	  f2.salario = 1700;
+	  f2.departamento = "Departamento de fotos";
+	  em.adiciona(f2);
+	  
+	  em.mostraEmpregados();
+	  em.mostra();
+	  
+	  boolean contem(Funcionario f) {
+		  
+		}
+	 }
+	}
